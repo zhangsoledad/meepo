ee8e185c19f14b7218918db7038116651d029eb7
zhangsoledad <787953403@qq.com>
2018-10-21 08:41:53 +0800

    feat: add example

--- examples/example.rs
+++ examples/example.rs
@@ -2,13 +2,29 @@ extern crate meepo;
 extern crate tiny_keccak;
 
 use meepo::meepo;
+use std::{thread, time};
 
 fn main() {
-    let _ = factorial(20);
+    let now = time::Instant::now();
+    let fab30 = factorial(30);
+    println!("fab20 elapsed {:?}", now.elapsed());
+    // assert_eq!(fab20, 2432902008176640000);
+    let now = time::Instant::now();
+    let fab30 = factorial(30);
+    println!("fab20 cached elapsed {:?}", now.elapsed());
+
+    let now = time::Instant::now();
+    let _ = swl(100);
+    println!("swl excute elapsed {:?}", now.elapsed());
+
+    let now = time::Instant::now();
+    let _ = swl(100);
+    println!("swl cached elapsed {:?}", now.elapsed());
+
 }
 
 #[meepo(maxsize = 100)]
-fn factorial(n: u64) -> u64 {
+fn factorial(n: u128) -> u128 {
     let mut ret = n;
     let mut p = 1;
     while ret > 1 {
@@ -16,4 +32,11 @@ fn factorial(n: u64) -> u64 {
         ret -= 1;
     }
     p
+}
+
+
+#[meepo(maxsize = 100)]
+fn swl(n: u128) -> u128 {
+    thread::sleep(time::Duration::from_secs(1));
+    n
 }
